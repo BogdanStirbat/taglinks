22c60dc325cb3603e3a239e554afaaa98012d190
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2018-10-18 22:14:05 +0300

    initial commit

---
+++ src/main/java/com/bstirbat/taglinks/taglinks/TaglinksApplication.java
@@ -0,0 +1,12 @@
+package com.bstirbat.taglinks.taglinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaglinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaglinksApplication.class, args);
+	}
+}
