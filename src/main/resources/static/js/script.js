10db6145627fc56f56bfec9db9be21bd6d76b709
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2018-11-13 22:36:37 +0200

    basic filter links/tags when one is selected functionality

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,36 +1,8 @@
 $(document).ready(function() {
 
-    $.ajax({
-        url: "http://localhost:8080/link/all",
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            var linksHtml = '';
-            data.forEach(function(link) {
-                linksHtml += '<li class="li_item"' + 'data-id="' + link.id + '"data-url="' + link.url + '"data-description="' + link.description + '"> <a href="' + link.url + '">' + link.url + '</a></li>';
-            });
-            $('#list_links').html(linksHtml);
-        },
-        error: function() {
-            console.log('Error.');
-        }
-    });
+    loadAllLinks();
 
-    $.ajax({
-        url: 'http://localhost:8080/tag/all',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            var urlsHtml = '';
-            data.forEach(function(url) {
-                urlsHtml += '<li class="li_url"' + 'data-id="' + url.id + '"data-name="' + url.name + '">' + url.name + '</li>';
-            });
-            $('#list_tags').html(urlsHtml);
-        },
-        error: function() {
-            console.log('Error.');
-        }
-    });
+    loadAllTags();
 
     $(document).on("click", ".li_item", function(e) {
         e.preventDefault();
@@ -42,28 +14,109 @@ $(document).ready(function() {
         popup += '<div class="popup-content">';
         popup += '<p>' + url + '</p>'
         popup += '<p>' + description + '</p>'
-        popup += '<button class="popup-close">Close</button>';
+        popup += '<button class="popup-close" data-type="link">Close</button>';
         popup += '</div>';
         popup += '</div>';
+        loadTagsForLink(id);
         $(this).append(popup);
     });
 
-    $(document).on("click", ".li_url", function(e) {
+    $(document).on("click", ".li_tag", function(e) {
         var id = $(this).data("id");
         var name = $(this).data("name");
         var popup = '';
         popup += '<div class="popup">';
         popup += '<div class="popup-content">';
         popup += '<p>' + name + '</p>'
-        popup += '<button class="popup-close">Close</button>';
+        popup += '<button class="popup-close" data-type="tag">Close</button>';
         popup += '</div>';
         popup += '</div>';
+        loadLinksForTag(id);
         $(this).append(popup);
     });
 
     $(document).on("click", ".popup-close", function(e) {
         e.preventDefault();
+        var type = $(this).data("type");
+        if (type == 'link') {
+            loadAllTags();
+        }
+        if (type == 'tag') {
+            loadAllLinks();
+        }
         $(this).parent().parent().remove();
         return false;
     });
+
+    function loadAllLinks() {
+        $.ajax({
+            url: "http://localhost:8080/link/all",
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                computeAndAppendHtmlForLinks(data);
+            },
+            error: function() {
+                console.log('Error.');
+            }
+        });
+    }
+
+    function loadAllTags() {
+        $.ajax({
+            url: 'http://localhost:8080/tag/all',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                computeAndAppendHtmlForTags(data);
+            },
+            error: function() {
+                console.log('Error.');
+            }
+        });
+    }
+
+    function computeAndAppendHtmlForLinks(links) {
+        var linksHtml = '';
+        links.forEach(function(link) {
+            linksHtml += '<li class="li_item"' + 'data-id="' + link.id + '"data-url="' + link.url + '"data-description="' + link.description + '"> <a href="' + link.url + '">' + link.url + '</a></li>';
+        });
+        $('#list_links').html(linksHtml);
+    }
+
+    function computeAndAppendHtmlForTags(tags) {
+        var urlsHtml = '';
+        tags.forEach(function(tag) {
+            urlsHtml += '<li class="li_tag"' + 'data-id="' + tag.id + '"data-name="' + tag.name + '">' + tag.name + '</li>';
+        });
+        $('#list_tags').html(urlsHtml);
+    }
+
+    function loadTagsForLink(link_id) {
+        $.ajax({
+            url: 'http://localhost:8080/linktag/all/link/' + link_id,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                computeAndAppendHtmlForTags(data);
+            },
+            error: function() {
+                console.log('Error.');
+            }
+        });
+    }
+
+    function loadLinksForTag(tag_id) {
+        $.ajax({
+            url: 'http://localhost:8080/linktag/all/tag/' + tag_id,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                computeAndAppendHtmlForLinks(data);
+            },
+            error: function() {
+                console.log('Error.');
+            }
+        });
+    }
 });
